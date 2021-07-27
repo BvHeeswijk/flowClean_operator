9112a9a21367e6723fb19ba9a3b4930bcd538553
BvHeeswijk <bennievheeswijk@gmail.com>
2021-07-27 23:04:35 +0000

    main -> workspace

--- workspace.R
+++ workspace.R
@@ -1,43 +1,4 @@
 library(tercen)
-library(dplyr)
-library(tibble)
-library(tidyverse)
-library(flowCore)
-library(flowClean)
-
-# Minimum amount of cells is 30.000
-# QC cutoff value = 10.000
-
-
-matrix2flowset <- function(a_matrix){ 
-  
-  minRange <- matrixStats::colMins(a_matrix)
-  maxRange <- matrixStats::colMaxs(a_matrix)
-  rnge <- maxRange - minRange
-  
-  df_params <- data.frame(
-    name = colnames(a_matrix),
-    desc = colnames(a_matrix),
-    range = rnge,
-    minRange = minRange,
-    maxRange = maxRange
-  )
-  
-  params <- Biobase::AnnotatedDataFrame()
-  Biobase::pData(params) <- df_params
-  Biobase::varMetadata(params) <- data.frame(
-    labelDescription = c("Name of Parameter",
-                         "Description of Parameter",
-                         "Range of Parameter",
-                         "Minimum Parameter Value after Transformation",
-                         "Maximum Parameter Value after Transformation")
-  )
-  flowset <- flowCore::flowFrame(a_matrix, params)
-  
-  return(flowset)
-}
-
-library(tercen)
 library(dplyr, warn.conflicts = FALSE, quietly = TRUE)
 library(magrittr)
 library(tibble)
@@ -91,8 +52,7 @@ flowclean_QC <- function(flowframe, input.pars){
   return(ifelse(qc_list >= 10000, "fail", "pass"))
 }
 
-ctx <- tercenCtx(workflowId = "36321b1d18264d80a1106708ce034a11",
-                 stepId = "6bc01961-4601-420a-9067-eba8a427dddb")
+ctx <- tercenCtx()
 
 if(ctx$cnames[1] == "filename") {filename <- TRUE
 if(ctx$cnames[2] != "Time") stop("Time not detected in the second column.")
@@ -142,36 +102,3 @@ QC_allfiles <- lapply(filenames, function(x) {
 qc_df$QC_flag <- do.call(c, QC_allfiles)
 flowClean_QC <- cbind(qc_df, .ci = (0:(nrow(qc_df)-1)))
 ctx$addNamespace(flowClean_QC) %>% ctx$save()
-ctx <- tercenCtx(workflowId = "36321b1d18264d80a1106708ce034a11",
-                 stepId = "16291844-2e39-40ba-8891-b7111ebe88ad")
-
-time <- ctx$cselect(ctx$cnames[[1]])
-data <- ctx$as.matrix() %>% t()
-data <- as.matrix(cbind(data, time))
-# Testing if the amount of input cells are >30000
-if (nrow(data)<30000) {stop("flowClean requires >30000 cells to be measured.")}
-
-fc_frame <- matrix2flowset(data)
-
-cutoffvar = ifelse((ctx$op.value('cutoff') == 10), "median", as.double(ctx$op.value('cutoff')))
-
-qc_list <- 
-  flowClean::clean(
-    fc_frame,
-    #filePrefixWithDir = "QC_output",
-    #vectMarkers = "markercolumns",
-    #ext = "fcs",
-    binSize = 0.01,
-    nCellCutoff = 500,
-    #announce = TRUE,
-    cutoff = cutoffvar,
-    #diagnostic = TRUE,
-    fcMax = 1.3,
-    returnVector = TRUE,
-    nstable = 5
-  )
-
-flag <- ifelse(qc_list >= 10000, "fail", "pass")
-qc_df <- data.frame(flag, .ci = (0:(length(flag)-1)))
-qc_result <- ctx$addNamespace(qc_df)
-ctx$save(qc_result)
